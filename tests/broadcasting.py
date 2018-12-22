284a1eed808ac5fe94fce4c44711f506a8b393e6
John Li <johnli0135@gmail.com>
2018-12-22 16:15:26 -0500

    add broadcasting tests to broadcasting.py

--- tests/broadcasting.py
+++ tests/broadcasting.py
@@ -1,8 +1,18 @@
 from nptyping import *
 import numpy as np
 
-def f() -> None:
-    a = np.zeros((3, 4))
-    b = np.zeros(a.shape[0] + 1)
-    d = b + np.zeros(1 + a.shape[1])
+a = np.zeros((3, 4,))
+b = np.zeros(1)
+c = np.ones(2)
+d = b + c
+e = a + b
 
+# Can't unify 'array[3, 4]' with 'array[2]' (unbroadcastable dimensions)
+# f = a + c
+
+g = np.zeros((8, 1, 6, 1))
+h = np.ones((7, 1, 5))
+i = g * h # OK. i: array[8, 7, 6, 5]
+
+# Can't unify 'array[8, 7, 6]' with 'array[8, 7, 6, 5]' (unbroadcastable dimensions)
+# j = np.zeros((8, 7, 6)) + i
