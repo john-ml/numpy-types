4b1b64b48cbd923ccef7cca95c826cd79d43cac2
John Li <johnli0135@gmail.com>
2018-12-28 21:23:33 -0500

    add more examples to indexing.py

--- tests/indexing.py
+++ tests/indexing.py
@@ -2,5 +2,10 @@ from nptyping import *
 import numpy as np
 
 a = np.zeros((3, 4, 5, 6))
-b = a[0, :, 3:4]
-c = b + np.zeros((1, 2))
+b = a[0, :, 3:4] # OK. b : array[4, 1, 6]
+
+# Can't unify 'array[4, 1, 6]' with 'array[1, 2]' (unbroadcastable dimensions)
+# c = b + np.zeros((1, 2))
+
+# Too many indices for array
+c = b[:, :, :, :]
